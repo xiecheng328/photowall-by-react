9743006aa329f342fcfc3a27a118830367be014f
13136666328@163.com <13136666328@163.com>
2016-08-16 23:58:39 +0800

    完成图片点击操作
    1.完成中心图片的点击旋转
    2.完成其他图片动画移动到中心位置

--- components/Photowall.js
+++ components/Photowall.js
@@ -10,26 +10,59 @@
     }
 
     /*
+        获取0-30°之间的一个任意正负值
+    */
+    function get30DegRandom(){
+        return (Math.random() > 0.5 ? '' : '-') + Math.ceil(Math.random() * 30);
+    }
+
+    /*
         创建图片组件
     */
     var ImgFigure = React.createClass({
-
+        handleClick : function(e){
+            if(this.props.arrange.isCenter){
+                this.props.inverse();
+            }else{
+                this.props.center();
+            }
+            e.stopPropagation();
+            e.preventDefault();
+        },
 
         render : function(){
             var styleObj = {};
             //如果props属性中制定了这张图片的位置，则使用
-            if(this.props.arrage.pos){
-                styleObj = this.props.arrage.pos;
-
+            if(this.props.arrange.pos){
+                styleObj = this.props.arrange.pos;
             }
+ 
+            //如果图片的旋转角度有值并且不为0， 添加旋转角度
+            if(this.props.arrange.rotate){
+                (['MozTransform', 'msTransform', 'WebkitTransform', 'transform']).forEach(function (value) {
+                    styleObj[value] = 'rotate(' + this.props.arrange.rotate + 'deg)';
+                  }.bind(this));
+            }
+
+            var imgFigureClassName = 'img-figure';
+            imgFigureClassName += this.props.arrange.isInverse ? ' is-inverse' : '';
+
+            if(this.props.arrange.isCenter){
+                styleObj.zIndex = 11;
+            }
+
             return (
-                <figure className="img-figure" style={styleObj}>
+                <figure className={imgFigureClassName} style={styleObj} onClick={this.handleClick}>
                     <img src={this.props.data.imgUrl} alt={this.props.data.title}/>
                     <figcaption >
                         <h2 className="img-title">{this.props.data.title}</h2>
+                        <div className="img-back" onClick={this.handleClick}>
+                            <p>
+                                {this.props.data.desc}
+                            </p>
+                        </div>
                      </figcaption>
                 </figure>
-
             );
         }
     });
@@ -54,6 +87,20 @@
             }
         },
 
+        /*
+            翻转图片,返回闭包函数
+        */
+        inverse : function(index){
+            return function(){
+                var imgsArrangeArr = this.state.imgsArrangeArr;
+                imgsArrangeArr[index].isInverse = !imgsArrangeArr[index].isInverse;
+                this.setState({
+                    imgsArrangeArr : imgsArrangeArr
+                });
+            }.bind(this);
+        },
+
+
         //  重新布局所有图片，指定居中哪个图片
         rearange : function(centerIndex){
             var imgsArrangeArr = this.state.imgsArrangeArr,
@@ -73,21 +120,29 @@
 
                 imgsArrangeCenterArr = imgsArrangeArr.splice(centerIndex, 1);
 
-                //居中centerIndex的图片
-                imgsArrangeCenterArr[0].pos = centerPos;
+                //居中centerIndex的图片, 居中图片不需要旋转
+                imgsArrangeCenterArr[0] = {
+                    pos : centerPos,
+                    rotate : 0,
+                    isCenter : true
+                };
 
-                //取出要布局上侧的图片的状态信息
+               /* //取出要布局上侧的图片的状态信息
                 topImgSplicIndex = Math.ceil(Math.random() * (imgsArrangeArr.length - topImgNum));
                 imgsArrangeTopArr = imgsArrangeArr.splice(topImgSplicIndex, topImgNum);
 
                 //布局位于上侧的图片
                 imgsArrangeTopArr.forEach(function(value, index){
-                    imgsArrangeTopArr[index].pos = {
-                        top : getRangeRandom(vPosRangeTopY[0], vPosRangeTopY),
-                        left : getRangeRandom(vPosRangeX[0], vPosRangeX[1])
+                    imgsArrangeTopArr[index] = {
+                        pos : {
+                            top : getRangeRandom(vPosRangeTopY[0], vPosRangeTopY),
+                            left : getRangeRandom(vPosRangeX[0], vPosRangeX[1])
+                        },
+                        rotate : get30DegRandom(),
+                        isCenter : false
                     };
 
-                });
+                });*/
 
                 //布局左右两侧的图片
                 for(var i=0, j=imgsArrangeArr.length, k=j/2; i<j; i++){
@@ -99,9 +154,13 @@
                     }else{
                         hPosRangeLORX = hPosRangeRightSecX;
                     }
-                    imgsArrangeArr[i].pos = {
-                        top : getRangeRandom(hPosRangeY[0], hPosRangeY[1]),
-                        left : getRangeRandom(hPosRangeLORX[0], hPosRangeLORX[1])
+                    imgsArrangeArr[i] = {
+                        pos : {
+                            top : getRangeRandom(hPosRangeY[0], hPosRangeY[1]),
+                            left : getRangeRandom(hPosRangeLORX[0], hPosRangeLORX[1])
+                        },
+                        rotate : get30DegRandom(),
+                        isCenter : false
                     }
                 }
 
@@ -115,7 +174,14 @@
                 this.setState({
                     imgsArrangeArr : imgsArrangeArr
                 });
-
+        },
+        /*
+            居中对应的图片
+        */
+        center : function(index){
+            return function(){
+                this.rearange(index);
+            }.bind(this);
         },
         getInitialState : function(){
             return {
@@ -124,7 +190,11 @@
                         pos : {
                             left : '0',
                             top : '0'
-                        }
+                        },
+                        rotate : 0,
+                        isInverse : false,
+                        isCenter : false
+
                     }*/
 
                 ]
@@ -168,7 +238,6 @@
             this.rearange(0);
         },
 
-
         render: function(){
             var controllerUnits = [],
                 imgFigures = [];
@@ -178,13 +247,16 @@
                         pos : {
                             left : 0,
                             top : 0
-                        }
+                        },
+                        rotate : 0,
+                        isInverse : false,
+                        isCenter : false
                     }
                 }
-                imgFigures.push(<ImgFigure data={value} key={index} ref={'imgFigure' + index} arrage={this.state.imgsArrangeArr[index]}/>);
+                imgFigures.push(<ImgFigure data={value} key={index} ref={'imgFigure' + index} 
+                    arrange={this.state.imgsArrangeArr[index]} inverse={this.inverse(index)} 
+                    center={this.center(index)}/>);
             }.bind(this));
-
-
 
             return (
                 <section className="stage" ref="stage">
@@ -203,36 +275,5 @@
         <Photowall />,
         document.getElementById('container')
     );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 })();
